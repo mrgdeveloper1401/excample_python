254a5016975dff37964700af18e84ae2cbd9b82c
mohammad goodarzi <m.goodarzi606@gmail.com>
2023-10-10 16:38:04 +0330

    use library socket and ..

--- day.py
+++ day.py
@@ -5,3 +5,17 @@ d2 = datetime.date(2016,1,21)
 
 
 print(d2 - d1)
+
+
+
+# year = input('enter year: ')
+# month = input('enter month: ')
+# day = input('enter day: ')
+# d = datetime.date(int(year), int(month), int(day))
+# birth_day = datetime.date.today() - d
+# print(f"You are {int(birth_day.days / 365)} years old.")
+# print(birth_day)
+
+
+# print(d)
+# print(datetime.date.today())
---
+++ run_command.py
@@ -0,0 +1,52 @@
+import sys
+
+
+# print(sys.path.append)
+# print(sys.maxsize)
+# print(sys.executable) # مشاهده نسخه مشاهده شده در پایتون
+
+
+# print(sys.version)
+
+# print(sys.platform)
+
+# پارامتر های در زمان اجرا رو به ما نشان میدهد
+# print(sys.argv)
+
+
+
+# if sys.argv[1] == 'x':
+#     print('x')
+# else:
+#     sys.exit()
+
+
+# ------------------------------------------------------------------------
+import os
+
+# print(os.getcwd())
+
+# print(os.access('/', os.W_OK))
+
+# print(os.listdir('/home/mohammad/Music')) # به لیتس دایرکتوری ها درون هر پوشه را میتوان دید که چی هست
+# ---------------------------------------------------------------------------
+from subprocess import call
+
+# call(['ls', '-l'])
+# call('pwd',)
+
+
+# -----------------------------------------------------------------------------
+import getpass
+
+# print(getpass.getuser())
+# -------------------------------------------------------------------------
+import socket
+
+# print(socket.gethostbyname(socket.gethostname()))
+# ----------------------------------------------------------------------------
+
+
+
+
+
